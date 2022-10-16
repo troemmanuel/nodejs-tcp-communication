344d0e3589c0e6c984a8f8041c8cbd7677a85678
troemmanuel <trohkopeemmanuel@gmail.com>
2022-10-16 23:43:06 +0000

    Update server.js

--- src/debuger.js
+++ src/debuger.js
@@ -1,6 +1,8 @@
+const chalk = require("chalk");
+
 const flag = process.env.DEBUG || true;
 exports.LOG = function (...args){
     if(flag === true){
-        console.log(args.join());
+        console.log(chalk.blue(`LOG (${new Date().toDateString()} ${new Date().getHours()} H ${new Date().getMinutes()} Min ${new Date().getSeconds()} sec) - ${args.join()}`));
     }
 }
